cf4535c0756a164198ce8e68ce8af5f79310b47a
Mike Causer <mcauser@gmail.com>
2017-06-24 16:31:10 +1000

    upip preparation

---
+++ setup.py
@@ -0,0 +1,25 @@
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system.
+sys.path.pop(0)
+from setuptools import setup
+
+setup(
+    name='micropython-pcd8544',
+    py_modules=['pcd8544'],
+    version='1.0.0',
+    description='MicroPython library for the PCD8544 LCD, used by Nokia 5110 displays.',
+    long_description='This library lets you communicate with LCDs using the Philips PCD8544 84x48 monochrome LCD driver, for example the Nokia 5110 display.',
+    keywords='pcd8544 monochrome lcd nokia micropython',
+    url='https://github.com/mcauser/micropython-pcd8544',
+    author='Mike Causer',
+    author_email='mcauser@gmail.com',
+    maintainer='Mike Causer',
+    maintainer_email='mcauser@gmail.com',
+    license='MIT',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: Implementation :: MicroPython',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
